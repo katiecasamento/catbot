97c911152444e69dcc9325199260ce1e7464ec61
Katie Casamento <kcsmnt0@gmail.com>
2021-04-16 16:59:43 -0700

    Fixed crash on reactions to non-CatBot messages

--- src/catbot/events.py
+++ src/catbot/events.py
@@ -19,14 +19,15 @@ async def on_reaction_add(reaction: Reaction, user: User):
     emoji = reaction.emoji
     cat = catbot.config.cats[reaction.message.id]
 
-    if emoji in catbot.config.reactions:
-        catbot.config.reactions[emoji].append(cat)
-    else:
-        catbot.config.reactions[emoji] = [cat]
+    if cat is not None:
+        if emoji in catbot.config.reactions:
+            catbot.config.reactions[emoji].append(cat)
+        else:
+            catbot.config.reactions[emoji] = [cat]
 
-    if emoji == catbot.config.vote_emoji:
-        data = json.dumps({ "image_id": cat, "sub_id": "test", "value": 1 })
-        vote = requests.post(catbot.config.voteURL, data=data, headers=catbot.config.post_headers)
-        vote.raise_for_status()
-        reply = "vote #{id} sent, server message: {message}".format(**vote.json())
-        await reaction.message.channel.send(reply)
+        if emoji == catbot.config.vote_emoji:
+            data = json.dumps({ "image_id": cat, "sub_id": "test", "value": 1 })
+            vote = requests.post(catbot.config.voteURL, data=data, headers=catbot.config.post_headers)
+            vote.raise_for_status()
+            reply = "vote #{id} sent, server message: {message}".format(**vote.json())
+            await reaction.message.channel.send(reply)
